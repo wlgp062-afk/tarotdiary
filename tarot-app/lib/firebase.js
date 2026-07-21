f8948fe2cbc5309dae7f54ed08ef7bc65d044f0d
wlgp062-afk <wlgp062@gmail.com>
2026-07-21 17:29:44 +0900

    파이어베이스 설정 파일 최종 수정

--- tarot-app/lib/firebase.js
+++ tarot-app/lib/firebase.js
@@ -1,18 +1,20 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDZfZC-KhJpLygM0QdQqAXOHVo1ZupoXpA",
+  apiKey: "AIzaSyDZfZC-KhJpLygM0QdqQAxOHVo1ZupoXpA",
   authDomain: "tarot-diary-79838.firebaseapp.com",
   projectId: "tarot-diary-79838",
-  storageBucket: "tarot-diary-79838.appspot.com",
+  storageBucket: "tarot-diary-79838.firebasestorage.app",
   messagingSenderId: "360314638573",
   appId: "1:360314638573:web:dcdc5f6e38151b4a5abce4",
   measurementId: "G-C2M5ZC7YDL"
 };
 
-const app = initializeApp(firebaseConfig);
+//파이어베이스 중복 초기화 방지 코드 포함
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
+
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 export const db = getFirestore(app);
