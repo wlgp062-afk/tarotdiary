b05ef17981b7e817372ee6bb49de63ba5abf94d4
wlgp062-afk <wlgp062@gmail.com>
2026-07-21 17:30:47 +0900

    파이어베이스 프로젝트 ID에 맞게 설정 수정

--- tarot-app/lib/firebase.js
+++ tarot-app/lib/firebase.js
@@ -4,15 +4,14 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDZfZC-KhJpLygM0QdqQAxOHVo1ZupoXpA",
-  authDomain: "tarot-diary-79838.firebaseapp.com",
-  projectId: "tarot-diary-79838",
-  storageBucket: "tarot-diary-79838.firebasestorage.app",
+  authDomain: "tarotdiaryapi.firebaseapp.com",
+  projectId: "tarotdiaryapi",
+  storageBucket: "tarotdiaryapi.firebasestorage.app",
   messagingSenderId: "360314638573",
   appId: "1:360314638573:web:dcdc5f6e38151b4a5abce4",
   measurementId: "G-C2M5ZC7YDL"
 };
 
-//파이어베이스 중복 초기화 방지 코드 포함
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
 
 export const auth = getAuth(app);
